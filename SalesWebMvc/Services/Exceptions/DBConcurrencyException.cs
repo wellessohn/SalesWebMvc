990ed82cc035be35ea936e3957de8df3d9f99936
wellessohn <wellessohn@gmail.com>
2019-07-24 14:48:07 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DBConcurrencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DBConcurrencyException : ApplicationException
+    {
+        public DBConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
